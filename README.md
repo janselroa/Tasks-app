7bb3c690fea7d92110ab55cc572a1c0c8ca4bf23
jansel <janselroa2424@gmail.com>
2021-10-26 14:19:23 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+### Tasks app in Vue.js
+
+<img src="./vista_previa.jpg">
+<img src="./vista_previa2.jpg">
---
+++ assets/js/index.js
@@ -0,0 +1,38 @@
+new Vue({
+    el: "#app",
+    mounted() {
+        if (localStorage.getItem("tasks")) {
+            this.listTasks = JSON.parse(localStorage.getItem("tasks"));
+        }
+    },
+    data: {
+        listTasks: [{ title: "Example", description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea rerum quos eos earum voluptates necessitatibus cum quis ex sequi dolores hic magnam, unde inventore nostrum officia nobis, beatae repellat atque!", image: "/assets/img/example.jpg" }, { title: "Example", description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea rerum quos eos earum voluptates necessitatibus cum quis ex sequi dolores hic magnam, unde inventore nostrum officia nobis, beatae repellat atque!", image: "/assets/img/example2.jpg" }],
+        newTask: {},
+        find: "",
+    },
+    methods: {
+        addTasks() {
+            if (this.newTask.title !== undefined) {
+                this.listTasks.push(this.newTask);
+                this.newTask = {};
+                localStorage.setItem("tasks", JSON.stringify(this.listTasks));
+            }
+        },
+        deleteTask(e) {
+            const title = e.target.parentElement.children[0].textContent;
+            this.listTasks.splice(this.listTasks.findIndex(task => task.title == title), 1);
+            localStorage.setItem("tasks", JSON.stringify(this.listTasks));
+            console.log(this.listTasks);
+        }
+    },
+
+    computed: {
+        findTask() {
+            if (this.find != "") {
+                return this.listTasks.filter(task => task.title.toLowerCase().includes(this.find.toLowerCase()));
+            }
+            return this.listTasks
+        }
+    }
+
+})
